ad0a177ee52162fa8f7ce97d955a61a9ffe06d31
Gavin Wang <gavinyipeiwang@gmail.com>
2017-03-16 22:13:24 +1100

    First commit.

---
+++ fibMem.js
@@ -0,0 +1,19 @@
+const fib = (n) => (n === 0 || n === 1) ? n : fib(n - 1) + fib(n - 2)
+
+const fibMem = (n) => {
+  let cache = {}
+
+  const mem = (x, fn) => {
+    if (x in cache) return cache[x]
+    else {
+      cache[x] = fn(x)
+      return cache[x]
+    }
+  }
+  const helper = (x) => {
+    if (x === 0 || x === 1) return x
+    else return mem(x - 1, helper) + mem(x - 2, helper)
+  }
+
+  return helper(n)
+}
